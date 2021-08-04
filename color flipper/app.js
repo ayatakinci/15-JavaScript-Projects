cc54aa0eb5820c93565f91ef7f8a57a82880bb99
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2021-08-04 18:01:14 +0300

    bug fixed

--- color flipper/app.js
+++ color flipper/app.js
@@ -1,17 +1,15 @@
-const color = ["green", "red", "rgba(133,122,200)","#f15025"];
-
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
 
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber);
-    document.body.style.background = color[randomNumber];
-    color.textContent = color[randomNumber]
-    
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * color.length); 
+  return Math.floor(Math.random() * colors.length);
 }
