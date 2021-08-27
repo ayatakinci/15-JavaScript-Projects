8f11e4a0a64514b8db6c9ae07faa62f9b115d2bf
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2021-08-27 13:28:51 +0300

    slider update

---
+++ slider/app.js
@@ -0,0 +1,27 @@
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+slides.forEach(function(slide, index){
+    slide.getElementsByClassName.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener("click", function(){
+    counter++;
+    carousel();
+})
+prevBtn.addEventListener("click", function(){
+    counter--;
+    carousel();
+});
+
+
+function carousel() {
+    if (counter === slides.length) {
+      counter = 0;
+    }
+    if (counter < 0) {
+      counter = slides.length - 1;
+    }
+};
